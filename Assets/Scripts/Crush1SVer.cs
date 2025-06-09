374b2c598528d9b0d553010f5b555d8d2074f555
CneEtobicoke <adambro1031@proton.me>
2025-06-09 15:56:37 -0400

    poo poo

--- Assets/Scripts/Crush1SVer.cs
+++ Assets/Scripts/Crush1SVer.cs
@@ -2,19 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Crush1SVer : MonoBehaviour
 {
-    public float speed = 5f;
     // Start is called before the first frame update
     void Start()
     {
-        GetComponent<Rigidbody2D>().AddForce(Vector2.right * speed, ForceMode2D.Impulse);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 }
-
